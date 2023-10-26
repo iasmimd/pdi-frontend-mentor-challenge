30e33a9eb80756413e741a37e5da7ed2f7523d5a
iasmim <iasmim.dantas@softfocus.com.br>
2023-10-26 16:30:02 -0300

    chore: improving config files and angular files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./*.html'],
+    content: ['./src/**/*.{html,scss,ts}'],
     theme: {
-      screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
-      },
-      extend: {
-        colors: {
-          strongCyan: 'hsl(171, 66%, 44%)',
-          lightBlue: 'hsl(233, 100%, 69%)',
-          darkGrayishBlue: 'hsl(210, 10%, 33%)',
-          grayishBlue: 'hsl(201, 11%, 66%)',
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
         },
-        fontFamily: {
-          sans: ['Bai Jamjuree', 'sans-serif'],
+        extend: {
+            colors: {
+                primary: '#EA7317',
+                lightWhite: '#FFFBFF',
+                mustard: '#E1AD01',
+            },
+            fontFamily: {
+                sans: ['Bai Jamjuree', 'sans-serif'],
+            },
         },
-      },
     },
     plugins: [],
-  }
+}
